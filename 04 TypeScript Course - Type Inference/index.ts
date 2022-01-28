ede41f4f11d3e4ec771c6230498a33abb09e429e
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-01-28 15:51:18 +0530

    04 TypeScript Course - Type Inference

---
+++ 04 TypeScript Course - Type Inference/index.ts
@@ -0,0 +1,14 @@
+let guests = ['Aquib', 'Saquib', 'Numan', 'Saad', 22, true]
+console.log(typeof guests);
+
+
+// let myName = "Khizr"
+// myName = 1234
+// console.log(typeof myName);
+
+// let year 
+// year = 2022
+// year = "2022"
+// console.log(typeof year);
+
+// Type Inference
