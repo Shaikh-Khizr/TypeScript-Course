1300f5715d9857d39106d1a107cb237dc71f04ca
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-01-28 15:27:01 +0530

    03 TypeScript Course - Define Types

---
+++ 03 TypeScript Course - Define Types/index.ts
@@ -0,0 +1,24 @@
+// PRIMITIVE TYPES
+// string
+let myName: string = "Khizr"
+// number
+let year: number = 2022
+// boolean
+let isEnable: boolean
+isEnable = true
+// null
+// undefined
+// symbol
+
+// OBJECT TYPES
+// objects, arrays, classes
+
+// Types of Array
+let guests: string[] = ['Aquib', 'Saquib', 'Numan', 'Saad']
+
+// Types for a Function
+// let greetUser: (name: string) => string;
+
+const greetUser = function (name: string): string {
+    return "Hello" + name.toUpperCase()
+}
