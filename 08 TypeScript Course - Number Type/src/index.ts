253083e9e6215597a319e3181d308b4219648b42
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-02 17:27:51 +0530

    08 TypeScript Course - Number Type

---
+++ 08 TypeScript Course - Number Type/src/index.ts
@@ -0,0 +1,21 @@
+// let year: number
+// year = 2022
+// console.log(year);
+
+// let cost = 20.22 // A NUMBER type of TS can also hold floating number
+// console.log(cost);
+
+// 4 - 100
+let myBits: number = 0B100 // "0b" represent that this is binary number - A NUMBER type of TS can also hold binary number
+
+let myOctals: number = 0O100 // "0o" represent that this is octal number - A NUMBER type of TS can also hold octal number
+
+let myHexadecimals: number = 0X100 // "0x" represent that this is hexadecimal number - A NUMBER type of TS can also hold hexadecimal number
+
+console.log(myBits, myOctals, myHexadecimals);
+
+// let bigInt: number = 9007199254740991
+let bigInt: bigint = 9007199254740991n
+
+console.log(bigInt);
+
