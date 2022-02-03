e60b7fd97f0b8d4483cea7f5ceab71f30138a7e8
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-03 13:04:08 +0530

    10 TypeScript Course - undefined vs null

---
+++ 10 TypeScript Course - undefined vs null/src/index.ts
@@ -0,0 +1,43 @@
+// undefined vs null
+
+// let price: number; // Not yet defined anything so it is "undefined"
+// price = 10
+// console.log(typeof price);
+// price = undefined
+// console.log(typeof price);
+
+// let price = null; // Can be defined but it already have a value called "null"
+// console.log(price);
+// // price = null
+// console.log(price);
+
+// let price: undefined;
+// console.log(typeof price, price);
+
+// let price2: null;
+// console.log(typeof price2, price2);
+// price2 = null
+// console.log(typeof price2, price2);
+
+// let price3: number | undefined | null;
+// console.log(price3);
+// console.log(price3 == null);
+// console.log(price3 === null);
+// console.log(price3 == undefined);
+// console.log(price3 === undefined);
+
+// price3 = 333
+// console.log(price3);
+// price3 = undefined
+// console.log(price3);
+// price3 = null
+// console.log(price3);
+
+let price3: number | undefined | null;
+price3 = null
+console.log(price3);
+// console.log(price3 == null); // true
+// console.log(price3 === null); // true
+
+console.log(price3 == undefined); // true
+console.log(price3 === undefined); // false
