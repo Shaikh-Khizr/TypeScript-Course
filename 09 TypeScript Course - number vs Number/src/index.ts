eb46debddf8cfe0e1a4e6ca39e6dfaa596a8ed04
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-03 12:28:58 +0530

    09 TypeScript Course - number vs Number

---
+++ 09 TypeScript Course - number vs Number/src/index.ts
@@ -0,0 +1,9 @@
+// number vs Number
+let value = 1000 // Primitive Data Type
+let otherValue = new Number('abc') // Number Object
+
+console.log(value, otherValue);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NaN);
