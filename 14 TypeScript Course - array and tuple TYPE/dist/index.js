79e752ebdd079d1e494465f17f9026008dd6d58a
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-04 22:14:17 +0530

    14 TypeScript Course - array and tuple TYPE

---
+++ 14 TypeScript Course - array and tuple TYPE/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// ARRAY AND TUPLE TYPE
+let fruits;
+fruits = ['mango', 'orange', 'apple'];
+let person = ['khizr', 'silmsoft', 123456];
+// person[2] = 'sdafsdaf'  
+let color = [244, 255, 0];
+// color.push(255)
+// console.log(color[3]);
+// React hooks
+// const [ form, setForm ] = useState()
---
+++ 14 TypeScript Course - array and tuple TYPE/src/index.ts
@@ -0,0 +1,14 @@
+// ARRAY AND TUPLE TYPE
+
+let fruits: string[]
+fruits = ['mango', 'orange', 'apple']
+
+let person: [string, string, number] = ['khizr', 'silmsoft', 123456]
+// person[2] = 'sdafsdaf'  
+
+let color: [number, number, number] = [244, 255, 0]
+// color.push(255)
+// console.log(color[3]);
+
+// React hooks
+// const [ form, setForm ] = useState()
