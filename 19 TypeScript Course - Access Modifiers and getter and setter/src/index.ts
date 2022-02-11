ca01bb1a381b3564d751d325afb36eea4b15b358
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-11 13:57:19 +0530

    19 TypeScript Course - Access Modifiers and getter and setter

---
+++ 19 TypeScript Course - Access Modifiers and getter and setter/src/index.ts
@@ -0,0 +1,39 @@
+// CLASS with TypeScript
+
+class Coordinate {
+    protected x: number;
+    protected y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+
+    // public getX() {
+    //     return this.x
+    // }
+
+    protected abcd() { }
+}
+
+class MyCoordinate extends Coordinate {
+    // public getX() {
+    get anything() {
+        return this.x
+    }
+
+    // public setX(value: number) {
+    set anything(value: number) {
+        this.x = value
+    }
+}
+
+// const point = new Coordinate(44, 5)
+const point = new MyCoordinate(44, 5)
+// point.setX(55)
+
+// point.X = 55
+// console.log(point.X);
+
+point.anything = 55
+console.log(point.anything);
