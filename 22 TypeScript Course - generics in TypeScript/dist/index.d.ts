6713641390403f8effc643dbb8dc13156abdebb3
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-02-18 16:44:00 +0530

    22 TypeScript Course - generics in TypeScript

---
+++ 22 TypeScript Course - generics in TypeScript/dist/index.d.ts
@@ -0,0 +1,4 @@
+declare let arrNum: number[];
+declare let arrString: string[];
+declare function getRandomElement<T>(arr: T[]): T;
+declare let result: string;
---
+++ 22 TypeScript Course - generics in TypeScript/src/index.ts
@@ -0,0 +1,33 @@
+// Generics in TypeScript
+
+let arrNum = [23, 45, 643, 345, 123456]
+
+// function getRandomElementInt(arr:number[]) {
+//     const index = Math.floor(Math.random() * arr.length)
+//     return arr[index]
+// }
+// console.log(getRandomElementInt(arrNum));
+
+let arrString = ['a', 'b', 'c', 'd', 'e']
+
+// function getRandomElementString(arr:string[]) {
+//     const index = Math.floor(Math.random() * arr.length)
+//     return arr[index]
+// }
+// console.log(getRandomElementString(arrString));
+
+
+// function getRandomElement(arr:any[]) : any {
+function getRandomElement<T>(arr: T[]): T {
+    const index = Math.floor(Math.random() * arr.length)
+    return arr[index]
+}
+
+// console.log(getRandomElement(arrNum));
+// console.log(getRandomElement(arrString));
+let result: string
+// result = getRandomElement(arrString)
+// result = getRandomElement(arrString)
+// console.log(typeof result);
+getRandomElement(arrNum)
+getRandomElement(arrNum)
